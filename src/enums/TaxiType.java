27521c8e8293b1fea8ee8f32427f1ecd00e8e902
nurjigit <nurjigit@gmail.com>
2023-01-05 15:54:06 +0600

    Added enum TaxiType

---
+++ src/enums/TaxiType.java
@@ -0,0 +1,16 @@
+package enums;
+
+import java.math.BigDecimal;
+
+public enum TaxiType {
+    STANDART(new BigDecimal(100), new BigDecimal(125)),
+    COMFORT(new BigDecimal(120), new BigDecimal(300)),
+    BUSINESS(new BigDecimal(83), new BigDecimal(100));
+    private BigDecimal pricePerKm;
+    private BigDecimal priceForLanding;
+
+    TaxiType(BigDecimal pricePerKm, BigDecimal priceForLanding) {
+        this.pricePerKm = pricePerKm;
+        this.priceForLanding = priceForLanding;
+    }
+}
