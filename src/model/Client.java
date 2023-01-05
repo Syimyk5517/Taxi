c4aa45cae6321992c2d5d1d3ca914f0eb8a0e75a
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-01-05 14:53:00 +0600

    commit

--- src/model/Client.java
+++ src/model/Client.java
@@ -8,5 +8,52 @@ public class Client {
     private LocalDate dateOfBirth;
     private  String phoneNumber;
 
+    public Client(Long id, String fullname, LocalDate dateOfBirth, String phoneNumber) {
+        this.id = id;
+        this.fullname = fullname;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", fullname='" + fullname + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
--- src/model/License.java
+++ src/model/License.java
@@ -6,4 +6,43 @@ public class License {
     private  Long id;
     private LocalDate dateOfIssue;
     private  LocalDate expirationDAte;
+
+    public License(Long id, LocalDate dateOfIssue, LocalDate expirationDAte) {
+        this.id = id;
+        this.dateOfIssue = dateOfIssue;
+        this.expirationDAte = expirationDAte;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(LocalDate dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public LocalDate getExpirationDAte() {
+        return expirationDAte;
+    }
+
+    public void setExpirationDAte(LocalDate expirationDAte) {
+        this.expirationDAte = expirationDAte;
+    }
+
+    @Override
+    public String toString() {
+        return "License{" +
+                "id=" + id +
+                ", dateOfIssue=" + dateOfIssue +
+                ", expirationDAte=" + expirationDAte +
+                '}';
+    }
 }
