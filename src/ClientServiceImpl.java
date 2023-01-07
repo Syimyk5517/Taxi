843b00da6e0a248bc22a12b4399815b251809603
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-01-07 12:40:36 +0600

    test

--- src/ClientServiceImpl.java
+++ src/ClientServiceImpl.java
@@ -65,7 +65,7 @@ public class ClientServiceImpl implements ClientService {
     public Map<Integer, Client> getClientAge() {
         Map<Integer,Client>clientMap=new HashMap<>();
         for(Client client : database.getClients()){
-            clientMap.put(LocalDate.now().minusYears(client.getDateOfBirth().getYear()).getYear(),client);
+            clientMap.put(LocalDate.now().minusYears(client.getDateOfBirth().getYear()).getYear(), client);
         }
         return clientMap;
     }
